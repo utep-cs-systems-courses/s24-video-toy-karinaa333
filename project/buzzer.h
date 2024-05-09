807d8cb4b8f3d0a64e861a8aa6cbbd5fc2c854fb
karinaa333 <karinarivera@Karinas-Air-2.lan>
2024-05-09 12:39:36 -0600

    buzzer

---
+++ project/buzzer.h
@@ -0,0 +1,10 @@
+#ifndef buzzer_included
+#define buzzer_included
+
+void buzzer_init();
+void buzzer_set_period(short cycles);
+
+extern const short int song_a[4];
+//extern const short int tone;
+
+#endif
